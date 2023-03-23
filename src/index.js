d270362f3f447dc84e55eead38ba1c027c2251a8
Piera Bellelli <piera.bellelli@gmail.com>
2023-03-23 12:08:57 +0100

    change and adding JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+losAngelesDateElement.innerHTML = "Happy coding";
