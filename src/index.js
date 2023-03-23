cba247aadea38febfef18b9ad02a1f73fc40ee79
Piera Bellelli <piera.bellelli@gmail.com>
2023-03-23 16:47:15 +0100

    timezone

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement = losAngelesElement.querySelector("div .date");
-losAngelesDateElement.innerHTML = "Happy coding";
+function updateLsTime() {
+  //Los Angeles
+
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+setInterval(updateLsTime, 1000);
+
+function updateParisTime() {
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+setInterval(updateParisTime, 1000);
