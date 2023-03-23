3a95c725abee2652e991c628107d206e367df4fa
Piera Bellelli <piera.bellelli@gmail.com>
2023-03-23 17:16:53 +0100

    adding cities

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityName = cityTimeZone.replace("_", "").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
